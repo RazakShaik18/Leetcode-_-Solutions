d4a6cd92617707f3072d743346c355d04d667ecb
Razak_shaik <skabdulrazak18@gmail.com>
2026-06-25 17:42:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.4%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char a : s){
+            //case-01 pushing opening bracket to the stack 
+            if(a == '(' || a == '{' || a == '[' ){
+                st.push(a);
+            }
+            //cases-02 if any closing bracket comes check where the stack top contain open bracket or not 
+           
+            /*if(!st.empty() && ((a == ')' && st.top() != '(') || (a == ']' && st.top() != '[') || (a == '}' && st.top() != '{'))){
+                return false;
+            st.pop();
+            }*/
+            else {
+                if(st.empty()) return false;
+
+                if((a == ')' && st.top() != '(') ||
+                   (a == ']' && st.top() != '[') ||
+                   (a == '}' && st.top() != '{')) {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+        return  st.empty();
+        
+        
+        
+    }
+};

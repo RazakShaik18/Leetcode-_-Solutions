65a22e3c36d43d6b06d72a25d14c6ebb06509e65
Razak_shaik <skabdulrazak18@gmail.com>
2026-07-20 18:28:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.47%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool isUgly(int n) {
         if (n <= 0)return false;
@@ -14,5 +14,18 @@ public:
        }
        
        return n==1;   
+    }
+};*/
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0)return false;
+        while(n>1){
+            if(n%2==0)n=n/2;
+            else if(n%3==0)n=n/3;
+            else if(n%5==0)n=n/5;
+            else break;
+        }
+        return (n==1);
     }
 };

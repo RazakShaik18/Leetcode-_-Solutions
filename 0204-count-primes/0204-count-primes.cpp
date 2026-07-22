302a9530469c36d0d7d3ca19281a61450bedc8a1
Razak_shaik <skabdulrazak18@gmail.com>
2026-07-22 11:53:25 +0530

    Time: 167 ms (69.34%), Space: 132.6 MB (15.67%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-    int countPrimes(int n){
-        int count = 0;
-        vector<int>mark(n,0);
-        //mark[0] = 1;
-        if(n<=2){
-            return 0;
-        }
-        //mark[1]= 1;
-        for(int i =2; i*i<n; i++){
-            if(mark[i]==0){
-                for(int j = i*i ; j<n ; j+=i){
+    int countPrimes(int n) {
+
+        // Prime numbers exist only from 2 onwards.
+        if (n <= 2) return 0;
+
+        // mark[i] = 0 -> Prime candidate
+        // mark[i] = 1 -> Composite number
+        vector<int> mark(n, 0);
+
+        /*
+            Sieve of Eratosthenes
+
+            Idea:
+            Every time we discover a prime, we eliminate all of its multiples.
+            A multiple of a prime can never be prime.
+
+            We start marking from i*i because smaller multiples
+            (2*i, 3*i, ..., (i-1)*i) have already been eliminated
+            while processing smaller prime numbers.
+        */
+        for (int i = 2; i * i < n; i++) {
+
+            if (!mark[i]) {
+                for (int j = i * i; j < n; j += i) {
                     mark[j] = 1;
                 }
             }
         }
-        for(int  i = 2 ; i<n ; i++){
-            if(mark[i]==0){
-                count++;
-            }
+
+        // Count the numbers that survived the elimination process.
+        int primes = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (!mark[i])
+                primes++;
         }
 
-        return count;
-    }    
-
-};
+        return primes;
+    }
+}; 

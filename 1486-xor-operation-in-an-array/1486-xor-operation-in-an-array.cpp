53f5f6278ec2bc297f7cc2625cd33763bd9c0f63
Razak_shaik <skabdulrazak18@gmail.com>
2026-07-22 12:09:03 +0530

    Time: 0 ms (100%), Space: 9.1 MB (9.17%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int ans = 0;
+        //this logic create sc : O(1)
+        /*int ans = 0;
         for(int i = 0 ; i<n; i++ ){
-            int a = start +2 *i;
+            int a = start +2*i;
             ans ^= a;
+        }
+        return ans;*/
+        //------------------------------//
+        // This logic will create sc : O(N)
+        int ans = 0;
+        vector<int>nums(n);
+        for(int i = 0 ; i<n ; i++){
+            nums[i] = start +2 *i;
+            ans ^= nums[i];
         }
         return ans;
     }

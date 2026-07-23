5b91132ae6fd0e496a14e933300002d74d0c2847
Razak_shaik <skabdulrazak18@gmail.com>
2026-07-23 13:31:56 +0530

    Time: 0 ms (100%), Space: 8 MB (12.55%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int commonFactors(int a, int b) {
-        int ans = 0;
+        int ans = 0;//this method is brute-force method
         for(int i = 1 ; i<=min(a,b);i++){
             if(a%i==0 && b%i==0){
                 ans++;

ddb4dc099968ec44f36bb1eb9627f60d531f3182
Razak_shaik <skabdulrazak18@gmail.com>
2026-07-26 21:58:09 +0530

    Time: 0 ms (100%), Space: 42.5 MB (17.4%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,10 +1,28 @@
 class Solution {
     public int trailingZeroes(int n) {
+
         int count = 0;
-        while(n>0){
-            n/=5;
-            count+=n;
+
+        // Trailing zero = factor of 10
+        // 10 = 2 × 5
+        // Factors of 2 are always more than factors of 5 in n!,
+        // so counting factors of 5 gives the number of trailing zeros.
+
+        while (n > 0) {
+
+            // Divide by 5 to count numbers containing factor 5.
+            // Example (n = 100):
+            // 100/5   = 20
+            // 20/5    = 4  (same as 100/25)
+            // 4/5     = 0  (same as 100/125)
+            // Total = 20 + 4 = 24
+
+            n = n / 5;
+
+            // Add the count of factors of 5.
+            count += n;
         }
+
         return count;
     }
 }

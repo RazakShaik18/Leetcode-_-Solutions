962377b448fd47c47c09e91ba99f9878c3189561
Razak_shaik <skabdulrazak18@gmail.com>
2026-07-30 18:33:45 +0530

    Time: 4 ms (66%), Space: 13.4 MB (82.26%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,10 +3,9 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int>ans;
         for(int i = 0 ; i<nums.size()-n;i++){
-        int k = i;
-        int j = i + n;
-            ans.push_back(nums[k]);
-            ans.push_back(nums[j]);
+        
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
         }
         return ans;
         

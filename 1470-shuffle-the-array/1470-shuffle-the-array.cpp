592057f03055a09eaf9ad7462f36534a34e79c1b
Razak_shaik <skabdulrazak18@gmail.com>
2026-07-30 18:36:06 +0530

    Time: 0 ms (100%), Space: 13.5 MB (19.59%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>ans;
-        for(int i = 0 ; i<nums.size()-n;i++){
-        
+        vector<int> ans;
+
+        // nums has 2n elements
+        // First half  : nums[0 ... n-1]
+        // Second half : nums[n ... 2n-1]
+
+        // One iteration = One (x, y) pair
+        // Total pairs = n
+        // So loop runs only n times (nums.size() - n == n)
+
+        for (int i = 0; i < nums.size() - n; i++) {
+
+            // Take element from first half
             ans.push_back(nums[i]);
-            ans.push_back(nums[i+n]);
+
+            // Matching element is always n positions ahead
+            // j = i + n
+            ans.push_back(nums[i + n]);
         }
+
         return ans;
-        
     }
 };

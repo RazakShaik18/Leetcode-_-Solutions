7070807897051dc85d3036ee850c0b64879f37da
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-01 13:35:07 +0530

    Time: 3 ms (40.69%), Space: 9.4 MB (64.47%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+        for(char ch : s){
+            if(ch == 'i'){
+                reverse(ans.begin(),ans.end());
+                continue;
+            }
+            else{
+                ans += ch;
+            }
+        }
+        return ans;
+    }
+};

645e51dea39c6a183795030ba242349b7b342313
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-10 15:27:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (64.8%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n = jewels.size();
+        int s = stones.size();
+        int count = 0;
+        for(int i = 0; i<n;i++){
+            for(int j = 0 ; j<s;j++){
+                if(jewels[i] == stones[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};

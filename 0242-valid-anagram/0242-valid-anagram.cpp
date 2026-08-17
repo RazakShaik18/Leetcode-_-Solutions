f89fb69031162e35789ecf3ed1d5e534c1f8edd8
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-17 11:15:20 +0530

    Time: 0 ms (100%), Space: 10 MB (11.55%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char,int> mp;
+
+        for(auto ch : s){
+            mp[ch]++;
+        }
+
+        for(auto ch : t){
+            mp[ch]--;
+        }
+
+        for(auto &p : mp){
+            if(p.second != 0){
+                return false;
+            }
+        }
+        return true;
+         
+        
+    }
+};

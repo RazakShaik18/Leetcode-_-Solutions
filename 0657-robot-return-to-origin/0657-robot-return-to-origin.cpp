f5a24f86a12a98daddf5707ad0e4921505c081ba
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-18 20:58:35 +0530

    Time: 7 ms (9.08%), Space: 10.3 MB (46.4%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for(auto ch : moves){
+            if(ch == 'R'){
+                x++;
+            }
+            else if(ch == 'L'){
+                x--;
+            }
+            else if(ch == 'U'){
+                y++;
+            }
+            else if (ch == 'D'){
+                y--;
+            }
+        }
+        return x == 0 && y ==0;
+        
+    }
+};

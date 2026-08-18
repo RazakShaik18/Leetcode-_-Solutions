6a6ae48569b76b00376cf073a5e3082706e76ea0
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-18 15:07:40 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.25%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         int last = n-1;
          while(s[last] == ' ') {
-        last--;
+            last--;
         }
         int  count = 0;
         for(int i = last; i >= 0 && s[i] != ' '; i--) {

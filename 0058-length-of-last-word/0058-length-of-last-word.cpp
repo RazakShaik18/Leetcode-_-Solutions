0db1435b8b27d191dc106b493862562b7c2acc21
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-18 15:40:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.25%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n = s.size();
@@ -11,5 +11,14 @@ public:
             count++;
         }
         return count;
+    }
+};*/
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int end = s.find_last_not_of(' ');
+        int start = s.find_last_of(' ', end);
+
+        return end - start;
     }
 };

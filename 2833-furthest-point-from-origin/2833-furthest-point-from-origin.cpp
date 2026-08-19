9bdfd1ecc14c1766b23336785bf2d99e16712422
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-19 10:58:37 +0530

    Time: 0 ms (100%), Space: 9.3 MB (5.61%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int pos = 0;
+        int cnt = 0;
+
+        for (char ch : moves) {
+            if (ch == 'R') {
+                pos++;
+            }
+            else if (ch == 'L') {
+                pos--;
+            }
+            else {
+                cnt++;
+            }
+        }
+
+        return abs(pos) + cnt;
+    }
+};

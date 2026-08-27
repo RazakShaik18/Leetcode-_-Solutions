a7e4c908f180c8d92d95c28bb18704953ecba357
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-27 11:55:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (97.1%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        vector<int>freq(26,0);
-        for(char ch : sentence){
-            freq[ch-'a']++;
-        }
-        for(int i = 0 ; i<26;i++){
-            if(freq[i]==0){
+
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+
+            if(sentence.find(ch) == string::npos) {
                 return false;
             }
         }
+
         return true;
     }
 };

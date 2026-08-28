0ab377a5d1f3017ddc79b4652193cdbdee45ac77
Razak_shaik <skabdulrazak18@gmail.com>
2026-08-28 09:45:53 +0530

    Time: 11 ms (14.33%), Space: 8.6 MB (50.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int mySqrt(int x) {
         int sq = 0;
-        for(int i = 1; i<=x ; i++){
-            if(1ll*i*i<=x){
-                sq= i;
+        for(int i = 0 ; i <= x ; i++){
+            if(1ll * i * i <= x){
+                sq = i;
+                
             }
-            else{
-                break;
-            }
+            else break;
         }
         return sq;
         

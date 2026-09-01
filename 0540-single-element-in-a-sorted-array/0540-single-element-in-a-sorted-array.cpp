ad7972a842784488fad16fc479088dbb71c66368
Razak_shaik <skabdulrazak18@gmail.com>
2026-09-01 15:10:26 +0530

    Time: 34 ms (3.56%), Space: 31.3 MB (7.73%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        // Count frequency of every element
+        for (int x : nums) {
+            mp[x]++;
+        }
+
+        // Find the element that appears once
+        for (auto it : mp) {
+            if (it.second == 1) {
+                return it.first;
+            }
+        }
+
+        return -1;
+    }
+};

5c3c109249ed78a308d196cdc6869e522d98226f
Razak_shaik <skabdulrazak18@gmail.com>
2026-09-08 09:48:21 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,23 @@
+
 class Solution {
 public:
     int countCommas(int n) {
-        if(n<1000){
-            return 0;
-        }
-        else{
-            return n-1000+1;
-        }
+
+        // 1 to 999 -> no commas
+        if (n < 1000) // 998  -->
+            return 0; 
+
+        // Commas start from 1000.
+        // Every number from 1000 to n has 1 comma.
+        //
+        // Count numbers from 1000 to n:
+        // n - 1000 + 1
+
+        return n - 1000 + 1;
     }
 };
+/*
+
+**Memory trick:**
+`1000` = first number with comma → **count from 1000 to n** → `n - 1000 + 1`.
+*/

abba643e13adef7733d018d4b88e2fc8692ee77e
Razak_shaik <skabdulrazak18@gmail.com>
2026-09-22 22:33:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.65%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.cpp
+++ 3959-check-good-integer/3959-check-good-integer.cpp
@@ -3,23 +3,16 @@ public:
     bool checkGoodInteger(int n) {
         int digitsum = 0;
         int squaresum = 0;
-        int num = n;
-        
-        //first digitsum of the number(n)
-        while(n>0){
-            digitsum += n%10;
-            n/=10;
+
+        while(n > 0) {
+            int digit = n % 10;
+
+            digitsum += digit;
+            squaresum += digit * digit;
+
+            n /= 10;
         }
 
-        //squaresum of the digits in number(n)
-        while(num>0){
-            int digit = num%10;
-            squaresum += digit*digit;
-            num/=10;
-        }
-        if((squaresum - digitsum)>=50){
-            return true;
-        }
-        return false;
+        return (squaresum - digitsum) >= 50;
     }
 };

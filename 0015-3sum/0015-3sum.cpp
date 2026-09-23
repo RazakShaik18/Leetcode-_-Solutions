754e93f58a32f7a65561bde18fa577af77682149
Razak_shaik <skabdulrazak18@gmail.com>
2026-09-23 20:56:51 +0530

    Time: 40 ms (89.42%), Space: 29.2 MB (45.63%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+
+            // Skip duplicate values
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            // Since array is sorted, impossible to make 0
+            if (nums[i] > 0)
+                break;
+
+            // Smallest possible sum is already > 0
+            if (nums[i] + nums[i + 1] + nums[i + 2] > 0)
+                break;
+
+            // Largest possible sum is still < 0
+            if (nums[i] + nums[n - 2] + nums[n - 1] < 0)
+                continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.push_back({
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    });
+
+                    while (left < right &&
+                           nums[left] == nums[left + 1])
+                        left++;
+
+                    while (left < right &&
+                           nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
